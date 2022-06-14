efedc8c95d0fdfafce73cc3ca309ad183c521065
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-14 10:15:50 +0100

    initial commit

---
+++ src/test/java/com/basketbandit/rseye/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basketbandit.rseye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
