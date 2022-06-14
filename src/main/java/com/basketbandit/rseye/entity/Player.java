b9dc9f554d6dc62e5373ad7e479d61cac7c8ec5c
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-14 22:39:47 +0100

    impl player class

---
+++ src/main/java/com/basketbandit/rseye/entity/Player.java
@@ -0,0 +1,11 @@
+package com.basketbandit.rseye.entity;
+
+import com.basketbandit.rseye.entity.fragment.*;
+
+public class Player {
+    public PlayerInfo info;
+    public PlayerStats stats;
+    public PlayerQuests quests;
+    public PlayerInventory inventory;
+    public PlayerBank bank;
+}
