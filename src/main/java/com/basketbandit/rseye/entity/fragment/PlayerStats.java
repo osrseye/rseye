dba323c5ea13225b27405c04660dc7781316adb3
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-14 16:20:47 +0100

    refactor playerstats record

--- src/main/java/com/basketbandit/rseye/entity/fragment/PlayerStats.java
+++ src/main/java/com/basketbandit/rseye/entity/fragment/PlayerStats.java
@@ -2,5 +2,5 @@ package com.basketbandit.rseye.entity.fragment;
 
 import java.util.HashMap;
 
-public record PlayerStats(Integer totalLevel, HashMap<String, Integer> levels) {
+public record PlayerStats(Integer totalLevel, HashMap<String, Integer> stats) {
 }
