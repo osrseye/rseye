a118ffce0dd07969af77210f3cee9a4c06b14191
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-15 12:22:12 +0100

    add default constructor to player to prevent some exceptions when players don't exist

--- src/main/java/com/basketbandit/rseye/entity/Player.java
+++ src/main/java/com/basketbandit/rseye/entity/Player.java
@@ -2,10 +2,21 @@ package com.basketbandit.rseye.entity;
 
 import com.basketbandit.rseye.entity.fragment.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player {
     public PlayerInfo info;
     public PlayerStats stats;
     public PlayerQuests quests;
     public PlayerInventory inventory;
     public PlayerBank bank;
+
+    public Player() {
+        this.info = new PlayerInfo("", "", new HashMap<>());
+        this.stats = new PlayerStats(-1, new HashMap<>());
+        this.quests = new PlayerQuests(-1, new ArrayList<>());
+        this.inventory = new PlayerInventory(new ArrayList<>(), new HashMap<>());
+        this.bank = new PlayerBank(-1, new ArrayList<>(), new HashMap<>());
+    }
 }
