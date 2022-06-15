df1c15d0648e65707665fe170929dcef750bbd4a
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-15 10:51:34 +0100

    add asset manager / base64 json file of item icons

--- src/main/java/com/basketbandit/rseye/Application.java
+++ src/main/java/com/basketbandit/rseye/Application.java
@@ -1,13 +1,20 @@
 package com.basketbandit.rseye;
 
+import com.basketbandit.rseye.entity.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class Application {
+	public static HashMap<String, Player> players = new HashMap<>();
+
+	public Application() {
+		new AssetManager();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
---
+++ src/main/java/com/basketbandit/rseye/AssetManager.java
@@ -0,0 +1,30 @@
+package com.basketbandit.rseye;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+public class AssetManager {
+    public Logger log = LoggerFactory.getLogger(AssetManager.class);
+    public static HashMap<String, String> itemIcons = new HashMap<>();
+
+    public AssetManager() {
+        try(BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(ResourceUtils.getFile("classpath:static/img/icons-items-complete.json")), StandardCharsets.UTF_8))) {
+            Gson gson = new Gson();
+            JsonObject obj = gson.fromJson(r, JsonObject.class);
+            obj.keySet().forEach(key -> itemIcons.put(key, "data:image/png;base64, " + obj.get(key).getAsString()));
+            log.info("Successfully parsed " + itemIcons.keySet().size() + " item icons");
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
