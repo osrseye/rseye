9eb8c8d00cd9573adf03f2f4976f49a69bac81be
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-15 15:33:06 +0100

    implement basic site controller with a number of player based endpoints

--- src/main/java/com/basketbandit/rseye/rest/SiteController.java
+++ src/main/java/com/basketbandit/rseye/rest/SiteController.java
@@ -1,6 +1,9 @@
 package com.basketbandit.rseye.rest;
 
+import com.basketbandit.rseye.Application;
+import com.basketbandit.rseye.entity.Player;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,5 +12,63 @@ public class SiteController {
     @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("index");
+    }
+
+    @GetMapping("/player/{username}")
+    public ModelAndView getPlayer(@PathVariable("username") String username) {
+        Player player = Application.players.getOrDefault(username, new Player());
+        ModelAndView mv = new ModelAndView("/player/index");
+        mv.addObject("playerInfo", player.info);
+        mv.addObject("playerStats", player.stats);
+        mv.addObject("playerQuests", player.quests);
+        mv.addObject("playerEquipment", player.equipment);
+        mv.addObject("playerInventory", player.inventory);
+        mv.addObject("playerBank", player.bank);
+        return mv;
+    }
+
+    @GetMapping("/player/{username}/stats")
+    public ModelAndView getPlayerStats(@PathVariable("username") String username) {
+        Player player = Application.players.getOrDefault(username, new Player());
+        ModelAndView mv = new ModelAndView("/player/stats");
+        mv.addObject("playerStats", player.stats);
+        return mv;
+    }
+
+    @GetMapping("/player/{username}/equipment")
+    public ModelAndView getPlayerEquipment(@PathVariable("username") String username) {
+        Player player = Application.players.getOrDefault(username, new Player());
+        ModelAndView mv = new ModelAndView("/player/equipment");
+        mv.addObject("playerEquipment", player.equipment);
+        return mv;
+    }
+
+    @GetMapping("/player/{username}/inventory")
+    public ModelAndView getPlayerInventory(@PathVariable("username") String username) {
+        Player player = Application.players.getOrDefault(username, new Player());
+        ModelAndView mv = new ModelAndView("/player/inventory");
+        mv.addObject("playerInventory", player.inventory);
+        return mv;
+    }
+
+    @GetMapping("/player/{username}/bank")
+    public ModelAndView getPlayerBank(@PathVariable("username") String username) {
+        Player player = Application.players.getOrDefault(username, new Player());
+        ModelAndView mv = new ModelAndView("/player/bank");
+        mv.addObject("playerBank", player.bank);
+        return mv;
+    }
+
+    @GetMapping("/player/{username}/quests")
+    public ModelAndView getPlayerQuests(@PathVariable("username") String username) {
+        Player player = Application.players.getOrDefault(username, new Player());
+        ModelAndView mv = new ModelAndView("/player/quests");
+        mv.addObject("playerQuests", player.quests);
+        return mv;
+    }
+
+    @GetMapping("/map")
+    public ModelAndView map() {
+        return  new ModelAndView("map");
     }
 }
