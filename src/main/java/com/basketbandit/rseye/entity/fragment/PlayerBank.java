c4d2c6cb8b0f2c794a09e297ef19168d28c1fbec
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-15 12:20:49 +0100

    update playerbank/inventory to hold hashmaps of item icons

--- src/main/java/com/basketbandit/rseye/entity/fragment/PlayerBank.java
+++ src/main/java/com/basketbandit/rseye/entity/fragment/PlayerBank.java
@@ -1,7 +1,8 @@
 package com.basketbandit.rseye.entity.fragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-public record PlayerBank(Integer value, ArrayList<Map<Integer, Integer>> slots) {
+public record PlayerBank(Integer value, ArrayList<Map<Integer, Integer>> slots, HashMap<Integer, String> icons) {
 }
--- src/main/java/com/basketbandit/rseye/entity/fragment/PlayerInventory.java
+++ src/main/java/com/basketbandit/rseye/entity/fragment/PlayerInventory.java
@@ -1,7 +1,8 @@
 package com.basketbandit.rseye.entity.fragment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-public record PlayerInventory(ArrayList<Map<Integer, Integer>> slots) {
+public record PlayerInventory(ArrayList<Map<Integer, Integer>> slots, HashMap<Integer, String> icons) {
 }
