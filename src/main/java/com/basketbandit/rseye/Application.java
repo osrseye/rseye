1b211ce67bb647b7cf2d7e3cb15c592919da8d79
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-16 01:33:54 +0100

    make players hashmap concurrent

--- src/main/java/com/basketbandit/rseye/Application.java
+++ src/main/java/com/basketbandit/rseye/Application.java
@@ -4,11 +4,11 @@ import com.basketbandit.rseye.entity.Player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 public class Application {
-	public static HashMap<String, Player> players = new HashMap<>();
+	public static ConcurrentHashMap<String, Player> players = new ConcurrentHashMap<>();
 
 	public Application() {
 		new AssetManager();
