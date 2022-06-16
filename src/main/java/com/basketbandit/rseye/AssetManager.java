059ae69b1d35e67b5c2fff30cc2151d156ceada5
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-16 11:07:41 +0100

    move icon json file to external - jar having issues

--- src/main/java/com/basketbandit/rseye/AssetManager.java
+++ src/main/java/com/basketbandit/rseye/AssetManager.java
@@ -4,11 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.ResourceUtils;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -20,12 +18,12 @@ public class AssetManager {
     public static HashMap<String, String> itemIcons = new HashMap<>();
 
     public AssetManager() {
-        try(BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(ResourceUtils.getFile("classpath:static/img/icons-items-complete.json")), StandardCharsets.UTF_8))) {
+        try(BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream("./data/icons-items-complete.json"), StandardCharsets.UTF_8))) {
             Gson gson = new Gson();
             JsonObject obj = gson.fromJson(r, JsonObject.class);
             obj.keySet().forEach(key -> itemIcons.put(key, "data:image/png;base64, " + obj.get(key).getAsString()));
             log.info("Successfully parsed " + itemIcons.keySet().size() + " item icons");
-        } catch(IOException e) {
+        } catch(Exception e) {
             log.error("There was an issue loading assets: {}", e.getMessage(), e);
         }
 
