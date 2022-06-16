94b174582659e5c2a2f6e6878f3189cd94f9f4f0
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-16 23:45:39 +0100

    urlencode thymeleaf links as they were causing http 500 errors

--- src/main/java/com/basketbandit/rseye/entity/Item.java
+++ src/main/java/com/basketbandit/rseye/entity/Item.java
@@ -1,14 +1,19 @@
 package com.basketbandit.rseye.entity;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class Item {
     public String id;
     public String name;
+    public String urlName;
     public String icon;
     public int quantity = -1;
 
     public Item(String id, String name, String icon) {
         this.id = id;
         this.name = name;
+        this.urlName = URLEncoder.encode(name, StandardCharsets.UTF_8).replace("+", "%20");
         this.icon = icon;
     }
 
@@ -19,6 +24,7 @@ public class Item {
     public Item(Item item) {
         this.id = item.id;
         this.name = item.name;
+        this.urlName = item.urlName;
         this.icon = item.icon;
         this.quantity = item.quantity;
     }
