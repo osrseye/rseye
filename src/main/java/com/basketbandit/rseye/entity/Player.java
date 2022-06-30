52225b3e603def2fc37386bb6d925de1fc2c783d
BasketBandit <errorsfound@hotmail.co.jp>
2022-06-30 15:38:44 +0100

    fully initialise player stats, including boosted level and xp

--- src/main/java/com/basketbandit/rseye/entity/Player.java
+++ src/main/java/com/basketbandit/rseye/entity/Player.java
@@ -18,11 +18,128 @@ public class Player {
     public Player() {
         this.loginState = "LOGGED_OUT";
         this.info = new PlayerInfo("", "", "", new HashMap<>());
-        this.stats = new PlayerStats(-1, new HashMap<>());
-        this.quests = new PlayerQuests(-1, new ArrayList<>());
+        this.stats = new PlayerStats(32, new HashMap<>(){{
+            // initialising this map makes life easier
+            put("ATTACK", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("HITPOINTS", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("MINING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("STRENGTH", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("AGILITY", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("SMITHING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("DEFENCE", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("HERBLORE", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("FISHING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("RANGED", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("THIEVING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("COOKING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("PRAYER", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("CRAFTING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("FIREMAKING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("MAGIC", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("FLETCHING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("WOODCUTTING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("RUNECRAFT", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("SLAYER", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("FARMING", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("CONSTRUCTION", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+            put("HUNTER", new HashMap<>(){{
+                put("level", 0);
+                put("xp", 0);
+                put("boostedLevel", 0);
+            }});
+        }});
+        this.quests = new PlayerQuests(0, new ArrayList<>());
         this.equipment = new PlayerEquipment(new HashMap<>());
         this.inventory = new PlayerInventory(new ArrayList<>());
-        this.bank = new PlayerBank(-1, new ArrayList<>());
+        this.bank = new PlayerBank(0, new ArrayList<>());
         this.lastUpdate = System.currentTimeMillis();
     }
 
