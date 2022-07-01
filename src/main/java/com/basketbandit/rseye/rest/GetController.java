358ceb41c6d0b091cb79f0ce39728232e6252e11
BasketBandit <errorsfound@hotmail.co.jp>
2022-07-01 17:09:47 +0100

    add api get functions for each player fragment

--- src/main/java/com/basketbandit/rseye/rest/GetController.java
+++ src/main/java/com/basketbandit/rseye/rest/GetController.java
@@ -2,7 +2,7 @@ package com.basketbandit.rseye.rest;
 
 import com.basketbandit.rseye.Application;
 import com.basketbandit.rseye.entity.Player;
-import com.basketbandit.rseye.entity.fragment.PlayerInfo;
+import com.basketbandit.rseye.entity.fragment.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +20,36 @@ public class GetController {
     @GetMapping("/api/v1/player/{username}/login_state")
     public String getPlayerLoginState(@PathVariable("username") String username) {
         return Application.players.getOrDefault(username, new Player()).loginState();
+    }
+
+    @GetMapping("/api/v1/player/{username}/information")
+    public PlayerInfo getPlayerInformation(@PathVariable("username") String username) {
+        return Application.players.getOrDefault(username, new Player()).info();
+    }
+
+    @GetMapping("/api/v1/player/{username}/inventory")
+    public PlayerInventory getPlayerInventory(@PathVariable("username") String username) {
+        return Application.players.getOrDefault(username, new Player()).inventory();
+    }
+
+    @GetMapping("/api/v1/player/{username}/equipment")
+    public PlayerEquipment getPlayerEquipment(@PathVariable("username") String username) {
+        return Application.players.getOrDefault(username, new Player()).equipment();
+    }
+
+    @GetMapping("/api/v1/player/{username}/quests")
+    public PlayerQuests getPlayerQuests(@PathVariable("username") String username) {
+        return Application.players.getOrDefault(username, new Player()).quests();
+    }
+
+    @GetMapping("/api/v1/player/{username}/stats")
+    public PlayerStats getPlayerStats(@PathVariable("username") String username) {
+        return Application.players.getOrDefault(username, new Player()).stats();
+    }
+
+    @GetMapping("/api/v1/player/{username}/bank")
+    public PlayerBank getPlayerBank(@PathVariable("username") String username) {
+        return Application.players.getOrDefault(username, new Player()).bank();
     }
 
     @GetMapping("/api/v1/players")
