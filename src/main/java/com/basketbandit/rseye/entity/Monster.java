496dfd7941722e6bb9d0ae409806a7783a4bb2d7
BasketBandit <errorsfound@hotmail.co.jp>
2022-11-04 16:38:17 +0000

    refactor Monster, added identifier function - returns string of "monster (level-value)"

--- src/main/java/com/basketbandit/rseye/entity/Monster.java
+++ src/main/java/com/basketbandit/rseye/entity/Monster.java
@@ -1,4 +1,7 @@
 package com.basketbandit.rseye.entity;
 
 public record Monster(String id, String name, int combatLevel) {
+    public String identifier() {
+        return "%s (level-%s)".formatted(name, combatLevel);
+    }
 }
