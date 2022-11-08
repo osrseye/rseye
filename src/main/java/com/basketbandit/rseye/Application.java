d82259e743748c1b6c7aa6292e69f6aa709b16a5
BasketBandit <errorsfound@hotmail.co.jp>
2022-11-08 00:57:17 +0000

    add new ping job to schedule handler

--- src/main/java/com/basketbandit/rseye/Application.java
+++ src/main/java/com/basketbandit/rseye/Application.java
@@ -7,8 +7,10 @@ import com.basketbandit.rseye.entity.event.GrowthEvent;
 import com.basketbandit.rseye.entity.event.QuestEvent;
 import com.basketbandit.rseye.entity.event.RaidEvent;
 import com.basketbandit.rseye.scheduler.ScheduleHandler;
+import com.basketbandit.rseye.scheduler.jobs.PingJob;
 import com.basketbandit.rseye.scheduler.jobs.UpdateJob;
 import com.basketbandit.rseye.scheduler.tasks.EventLogClearTask;
+import com.basketbandit.rseye.scheduler.tasks.PingWebSocketTask;
 import com.basketbandit.rseye.scheduler.tasks.PlayerStateCheckTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,6 +30,7 @@ public class Application {
 	public Application() {
 		new AssetManager();
 		ScheduleHandler.registerJob(new UpdateJob(new PlayerStateCheckTask(), new EventLogClearTask()));
+		ScheduleHandler.registerJob(new PingJob(new PingWebSocketTask()));
 	}
 
 	public static void main(String[] args) {
