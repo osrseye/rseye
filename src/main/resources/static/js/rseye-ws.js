b56022cd208d92929c0bd46d38f3273efc6c415f
BasketBandit <errorsfound@hotmail.co.jp>
2025-07-18 19:01:41 +0100

    refactor websocket

--- src/main/resources/static/js/rseye-ws.js
+++ src/main/resources/static/js/rseye-ws.js
@@ -16,48 +16,53 @@ function connect() {
     ws.onmessage = function(event) {
         const data = event.data;
 
-        if(data == "ping") {
-            send("pong");
-            return;
+        if(data === "ping") {
+            return send("pong");
         }
 
-        if(data.startsWith("fetch:")) {
-            // in case websocket connection is lost, clear old data
-            $(".player-online").empty();
-            $(".player-offline").empty();
-            $(".map-player-online").empty();
-            $(".map-player-offline").empty();
-            $(".leaflet-marker-pane").empty(); // experimental
-
-            // load each player
-            $.each(JSON.parse(data.substring("fetch:".length, data.length)), function(username, player) {
-                $.get("/player/"+player.username.natural, function(data) {
-                    $(data.includes("LOGGED_IN") ? ".player-online" : ".player-offline").append(data);
-                    $("#map-status-"+player.username.encoded).detach().appendTo(data.includes("LOGGED_IN") ? ".map-player-online" : ".map-player-offline");
-                    $('[data-toggle="tooltip"]').tooltip() // initialise tooltips
-
-                    // add to world map
-                    worldMap.addPlayerMarker(player, "world");
-
-                    // create minimap
-                    playerMinimaps.set(player.username.encoded, new RuneMap(player.username.encoded + "-minimap"));
-                    minimap = playerMinimaps.get(player.username.encoded);
-                    minimap.setView(player.position.offx, player.position.offy);
-                    minimap.updateLayer(player.position.plane);
-                    minimap.addPlayerMarker(player, "mini");
-
-                    updatePosition(player);
-                });
-            });
-            return;
+        const index = data.indexOf(":");
+        if(index === -1) {
+            return console.warn("Malformed message:", data);
         }
 
-        if(data.startsWith("new_player:")) {
-            const player = JSON.parse(data.substring("new_player:".length, data.length));
+        const type = data.substring(0, index);
+        const payload = data.substring(index + 1);
+        switch(type) {
+            case "fetch": return fetch(payload);
+            case "new_player": return newPlayer(payload);
+            case "login_update": return loginUpdate(payload);
+            case "position_update": return positionUpdate(payload);
+            case "combat_loot_update": return combatLootUpdate();
+            case "raid_loot_update": return raidLootUpdate();
+            case "skill_update": return skillUpdate();
+            case "skill_data": return skillData(payload);
+            case "quest_update": return questUpdate();
+            case "exp_update": return expUpdate(payload);
+            case "inventory_update": return inventoryUpdate(payload);
+            case "bank_update": return bankUpdate(payload);
+            case "equipment_update": return equipmentUpdate(payload);
+            case "status_update": return statusUpdate(payload);
+            case "quest_data": return questData(payload);
+            case "overhead_update": return overheadUpdate(payload);
+            case "skull_update": return skullUpdate(payload);
+            default: return console.warn("Unhandled message type:", type);
+        }
+    };
+
+    function fetch(payload) {
+        // in case websocket connection is lost, clear old data
+        $(".player-online").empty();
+        $(".player-offline").empty();
+        $(".map-player-online").empty();
+        $(".map-player-offline").empty();
+        $(".leaflet-marker-pane").empty(); // experimental
+
+        // load each player
+        $.each(JSON.parse(payload), function(username, player) {
             $.get("/player/"+player.username.natural, function(data) {
                 $(data.includes("LOGGED_IN") ? ".player-online" : ".player-offline").append(data);
                 $("#map-status-"+player.username.encoded).detach().appendTo(data.includes("LOGGED_IN") ? ".map-player-online" : ".map-player-offline");
-                $("#"+player.username.encoded+"-position").detach().appendTo('#canvas-container');
+                $('[data-toggle="tooltip"]').tooltip() // initialise tooltips
 
                 // add to world map
                 worldMap.addPlayerMarker(player, "world");
@@ -71,168 +76,172 @@ function connect() {
 
                 updatePosition(player);
             });
-            return;
-        }
+        });
+    }
 
-        if(data.startsWith("login_update:")) {
-            const player = JSON.parse(data.substring("login_update:".length, data.length));
-            $.get("/api/v2/player/"+player.username.natural+"/login_state", function(data) {
-                const pn = $("#"+player.username.encoded);
-                const map = $("#map-status-"+player.username.encoded);
-                const badge = pn.find(".badge");
-                const mapBadge = map.find(".badge");
-                if((data == "LOGGED_IN" || data == "HOPPING") && badge.hasClass("badge-danger")) {
-                    badge.removeClass("badge-danger").addClass("badge-success").text("Online");
-                    pn.detach().appendTo(".player-online");
-                    mapBadge.removeClass("badge-danger").addClass("badge-success").text("Online");
-                    map.detach().appendTo(".map-player-online");
-                    return;
-                }
-                badge.removeClass("badge-success").addClass("badge-danger").text("Offline");
-                pn.detach().appendTo(".player-offline");
-                mapBadge.removeClass("badge-success").addClass("badge-danger").text("Offline");
-                map.detach().appendTo(".map-player-offline");
-            });
-            return;
-        }
+    function newPlayer(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural, function(data) {
+            $(data.includes("LOGGED_IN") ? ".player-online" : ".player-offline").append(data);
+            $("#map-status-"+player.username.encoded).detach().appendTo(data.includes("LOGGED_IN") ? ".map-player-online" : ".map-player-offline");
+            $("#"+player.username.encoded+"-position").detach().appendTo('#canvas-container');
 
-        if(data.startsWith("position_update:")) {
-            const player = JSON.parse(data.substring("position_update:".length, data.length));
+            // add to world map
+            worldMap.addPlayerMarker(player, "world");
+
+            // create minimap
+            playerMinimaps.set(player.username.encoded, new RuneMap(player.username.encoded + "-minimap"));
+            minimap = playerMinimaps.get(player.username.encoded);
+            minimap.setView(player.position.offx, player.position.offy);
+            minimap.updateLayer(player.position.plane);
+            minimap.addPlayerMarker(player, "mini");
+
             updatePosition(player);
-            return;
-        }
+        });
+    }
 
-        if(data.startsWith("combat_loot_update:")) {
-            $.get("/combat/latest", function(data) {
-                $(".update-feed").css({top:-150});
-                $(".update-feed").prepend(data);
-                $(".update-feed").animate({top: 5}, 1000);
-                clearFeed();
-            });
-            $.get("/loot-tracker", function(data) {
-                $("#global-loot-tracker").html(data);
-            })
-            return;
-        }
-
-        if(data.startsWith("raid_loot_update:")) {
-            $.get("/raid/latest", function(data) {
-                $(".update-feed").css({top:-150});
-                $(".update-feed").prepend(data);
-                $(".update-feed").animate({top: 5}, 1000);
-                clearFeed();
-            });
-            return;
-        }
-
-        if(data.startsWith("skill_update:")) {
-            $.get("/growth/latest", function(data) {
-                $(".update-feed").prepend(data);
-                clearFeed();
-            });
-            return;
-        }
-
-        if(data.startsWith("quest_update:")) {
-            $.get("/quest/latest", function(data) {
-                $(".update-feed").prepend(data);
-                clearFeed();
-            });
-            return;
-        }
-
-        if(data.startsWith("exp_update")) {
-            const player = JSON.parse(data.substring("exp_update:".length, data.length));
-
-            var updateString = "";
-            for(const [key, value] of Object.entries(player.data)) {
-                updateString += "<img class='xp-drop-icon' src='/data/icons/skill/"+key+".png'/><span>"+value+"</span><br>";
+    function loginUpdate(payload) {
+        const player = JSON.parse(payload);
+        $.get("/api/v2/player/"+player.username.natural+"/login_state", function(data) {
+            const pn = $("#"+player.username.encoded);
+            const map = $("#map-status-"+player.username.encoded);
+            const badge = pn.find(".badge");
+            const mapBadge = map.find(".badge");
+            if((data == "LOGGED_IN" || data == "HOPPING") && badge.hasClass("badge-danger")) {
+                badge.removeClass("badge-danger").addClass("badge-success").text("Online");
+                pn.detach().appendTo(".player-online");
+                mapBadge.removeClass("badge-danger").addClass("badge-success").text("Online");
+                map.detach().appendTo(".map-player-online");
+                return;
             }
+            badge.removeClass("badge-success").addClass("badge-danger").text("Offline");
+            pn.detach().appendTo(".player-offline");
+            mapBadge.removeClass("badge-success").addClass("badge-danger").text("Offline");
+            map.detach().appendTo(".map-player-offline");
+        });
+    }
 
-            var updateWorld = $("<span class='xp-drop'>" + updateString + "</span>");
-            var updateMini = $("<span class='xp-drop'>" + updateString + "</span>");
-            $("div[title='"+player.username.encoded+"-world']").append(updateWorld);
-            $("div[title='"+player.username.encoded+"-mini']").append(updateMini);
-            updateWorld.css({top:(-15+-updateWorld.height())});
-            updateWorld.animate({
-                opacity: '0',
-                top: -225+-updateWorld.height()
-            }, 3000, function(){
-              updateWorld.remove();
-            });
-            updateMini.css({top:(-15+-updateMini.height())});
-            updateMini.animate({
-                opacity: '0',
-                top: -225+-updateMini.height()
-            }, 3000, function(){
-              updateMini.remove();
-            });
-            return;
+    function positionUpdate(payload) {
+        const player = JSON.parse(payload);
+        updatePosition(player);
+    }
+
+    function expUpdate(payload) {
+        const player = JSON.parse(payload);
+
+        var updateString = "";
+        for(const [key, value] of Object.entries(player.data)) {
+            updateString += "<img class='xp-drop-icon' src='/data/icons/skill/"+key+".png'/><span>"+value+"</span><br>";
         }
 
-        if(data.startsWith("inventory_update:")) {
-            const player = JSON.parse(data.substring("inventory_update:".length, data.length));
-            $.get("/player/"+player.username.natural+"/inventory", function(data) {
-                updatePlayerContainer(".inventory-container", player, data);
-            });
-            return;
-        }
+        var updateWorld = $("<span class='xp-drop'>" + updateString + "</span>");
+        var updateMini = $("<span class='xp-drop'>" + updateString + "</span>");
+        $("div[title='"+player.username.encoded+"-world']").append(updateWorld);
+        $("div[title='"+player.username.encoded+"-mini']").append(updateMini);
+        updateWorld.css({top:(-15+-updateWorld.height())});
+        updateWorld.animate({
+            opacity: '0',
+            top: -225+-updateWorld.height()
+        }, 3000, function(){
+          updateWorld.remove();
+        });
+        updateMini.css({top:(-15+-updateMini.height())});
+        updateMini.animate({
+            opacity: '0',
+            top: -225+-updateMini.height()
+        }, 3000, function(){
+          updateMini.remove();
+        });
+    }
 
-        if(data.startsWith("bank_update:")) {
-            const player = JSON.parse(data.substring("bank_update:".length, data.length));
-            $.get("/player/"+player.username.natural+"/bank", function(data) {
-                updatePlayerContainer(".bank-container", player, data);
-            });
-            return;
-        }
+    function skillUpdate() {
+        $.get("/growth/latest", function(data) {
+            $(".update-feed").prepend(data);
+            clearFeed();
+        });
+    }
 
-        if(data.startsWith("equipment_update:")) {
-            const player = JSON.parse(data.substring("equipment_update:".length, data.length));
-            $.get("/player/"+player.username.natural+"/equipment", function(data) {
-                updatePlayerContainer(".equipment-container", player, data);
-            });
-            return;
-        }
+    function skillData(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural+"/skills", function(data) {
+            updatePlayerContainer(".skills-container", player, data);
+        });
+    }
 
-        if(data.startsWith("status_update")) {
-            // loads the player current hitpoints/prayer
-            const player = JSON.parse(data.substring("status_update:".length, data.length));
-            $.get("/player/"+player.username.natural+"/status", function(data) {
-                $("#map-status-"+player.username.encoded).find(".player-current-state").replaceWith(data);
-            });
-            return;
-        }
+    function questUpdate() {
+        $.get("/quest/latest", function(data) {
+            $(".update-feed").prepend(data);
+            clearFeed();
+        });
+    }
 
-        if(data.startsWith("skill_data:")) {
-            const player = JSON.parse(data.substring("skill_data:".length, data.length));
-            $.get("/player/"+player.username.natural+"/skills", function(data) {
-                updatePlayerContainer(".skills-container", player, data);
-            });
-            return;
-        }
+    function questData(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural+"/quests", function(data) {
+            updatePlayerContainer(".quests-container", player, data);
+        });
+    }
 
-        if(data.startsWith("quest_data:")) {
-            const player = JSON.parse(data.substring("quest_data:".length, data.length));
-            $.get("/player/"+player.username.natural+"/quests", function(data) {
-                updatePlayerContainer(".quests-container", player, data);
-            });
-            return;
-        }
+    function inventoryUpdate(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural+"/inventory", function(data) {
+            updatePlayerContainer(".inventory-container", player, data);
+        });
+    }
 
-        if(data.startsWith("overhead_update:")) {
-            const player = JSON.parse(data.substring("overhead_update:".length, data.length));
-            $("#map-status-"+player.username.encoded).find(".player-overheads").children().hide();
-            $("#map-status-"+player.username.encoded).find("."+player.overhead).show();
-            return;
-        }
+    function equipmentUpdate(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural+"/equipment", function(data) {
+            updatePlayerContainer(".equipment-container", player, data);
+        });
+    }
 
-        if(data.startsWith("skull_update:")) {
-            const player = JSON.parse(data.substring("skull_update:".length, data.length));
-            $("#map-status-"+player.username.encoded).find(".player-skulls").children().hide();
-            $("#map-status-"+player.username.encoded).find("."+player.skull).show();
-            return;
-        }
-    };
+    function bankUpdate(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural+"/bank", function(data) {
+            updatePlayerContainer(".bank-container", player, data);
+        });
+    }
+
+    function statusUpdate(payload) {
+        const player = JSON.parse(payload);
+        $.get("/player/"+player.username.natural+"/status", function(data) {
+            $("#map-status-"+player.username.encoded).find(".player-current-state").replaceWith(data);
+        });
+    }
+
+    function overheadUpdate(payload) {
+        const player = JSON.parse(payload);
+        $("#map-status-"+player.username.encoded).find(".player-overheads").children().hide();
+        $("#map-status-"+player.username.encoded).find("."+player.overhead).show();
+    }
+
+    function skullUpdate(payload) {
+        const player = JSON.parse(payload);
+        $("#map-status-"+player.username.encoded).find(".player-skulls").children().hide();
+        $("#map-status-"+player.username.encoded).find("."+player.skull).show();
+    }
+
+    function combatLootUpdate() {
+        $.get("/combat/latest", function(data) {
+            $(".update-feed").css({top:-150});
+            $(".update-feed").prepend(data);
+            $(".update-feed").animate({top: 5}, 1000);
+            clearFeed();
+        });
+        $.get("/loot-tracker", function(data) {
+            $("#global-loot-tracker").html(data);
+        })
+    }
+
+    function raidLootUpdate() {
+        $.get("/raid/latest", function(data) {
+            $(".update-feed").css({top:-150});
+            $(".update-feed").prepend(data);
+            $(".update-feed").animate({top: 5}, 1000);
+            clearFeed();
+        });
+    }
 }
 
 function send(data) {
